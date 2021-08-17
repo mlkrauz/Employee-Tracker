3169ab033ca5a26dca8e21a6d6863277a24ae505
ElusiveEntity <30778577+ElusiveEntity@users.noreply.github.com>
2021-08-17 11:53:34 -0400

    Added server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+//Imports
+const express = require('express');
+const mysql = require('mysql2');
+
+//Launch express & middleware
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
