064a44d904a36142dcc3866aef666e82a057b2b9
mlkrauz <AnElusiveEntity@gmail.com>
2021-10-02 22:22:31 -0400

    Designed lib architecture.

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-//Imports
-const express = require('express');
-const mysql = require('mysql2');
+//Imports;
 require('dotenv').config();
 
 //Launch express & middleware
 const PORT = process.env.PORT || 3001;
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+
 
